edf6a1fd13f269fd080f2915817bd5a1e399a669
tsmsogn <tsmsogn@gmail.com>
2017-04-18 10:14:19 +0900

    Remove samples

--- sensingWithMediaCapture/src/main/java/com/ubhave/example/capturemedia/HiddenConstants.java
+++
@@ -1,8 +0,0 @@
-package com.ubhave.example.capturemedia;
-
-class HiddenConstants {
-    public static final String YOUR_SERVERS_FILE_POST_URL = "";
-    public static final String YOUR_SERVERS_RESPONSE_ON_SUCCESSFUL_POST = "";
-    public static final String YOUR_POST_PARAM_VALUE = "";
-    public static final String YOUR_POST_PARAM_KEY = "";
-}
