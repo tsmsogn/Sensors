c743c6165a6936b6e5818baa011b6a8678e4eb82
tsmsogn <tsmsogn@gmail.com>
2017-04-19 10:06:50 +0900

    Create some data loggers

---
+++ sensorManagerExample/src/main/java/com/ubhave/sensormanager/tester/loggers/RemoteServerDetails.java
@@ -0,0 +1,9 @@
+package com.ubhave.sensormanager.tester.loggers;
+
+class RemoteServerDetails {
+    public static final String FILE_POST_URL = "";
+    public static final String FILE_KEY = "";
+    public static final String RESPONSE_ON_SUCCESS = "";
+    public static final String API_KEY_KEY = "";
+    public static final String API_KEY_VALUE = "";
+}
---
+++ sensorManagerExample/src/main/java/com/ubhave/sensormanager/tester/loggers/StoreOnlyEncryptedFiles.java
@@ -0,0 +1,82 @@
+/* **************************************************
+Copyright (c) 2015
+Neal Lathia, @nlathia
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ ************************************************** */
+
+package com.ubhave.sensormanager.tester.loggers;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.ubhave.datahandler.config.DataStorageConfig;
+import com.ubhave.datahandler.except.DataHandlerException;
+import com.ubhave.datahandler.loggertypes.AbstractDataLogger;
+import com.ubhave.datahandler.loggertypes.AbstractStoreOnlyLogger;
+import com.ubhave.sensormanager.ESException;
+import com.ubhave.sensormanager.tester.ApplicationContext;
+
+public class StoreOnlyEncryptedFiles extends AbstractStoreOnlyLogger
+{
+	private static StoreOnlyEncryptedFiles instance;
+	
+	public static AbstractDataLogger getInstance() throws ESException, DataHandlerException
+	{
+		if (instance == null)
+		{
+			instance = new StoreOnlyEncryptedFiles(ApplicationContext.getContext());
+		}
+		return instance;
+	}
+
+	protected StoreOnlyEncryptedFiles(final Context context) throws DataHandlerException, ESException
+	{
+		super(context, DataStorageConfig.STORAGE_TYPE_FILES);
+	}
+
+	@Override
+	protected String getFileStorageName()
+	{
+		return "Demo-Encrypted-File-Storage";
+	}
+
+	@Override
+	protected String getUniqueUserId()
+	{
+		// Note: this should not be a static string
+		return "test-user-id";
+	}
+
+	@Override
+	protected String getDeviceId()
+	{
+		// Note: this should not be a static string
+		return "test-device-id";
+	}
+
+	@Override
+	protected boolean shouldPrintLogMessages()
+	{
+		// Note: return false to turn off Log.d messages
+		return true;
+	}
+
+	@Override
+	protected String getEncryptionPassword()
+	{
+		Log.d("MainActivity", "getEncryptionPassword()");
+		// Note: return non-null password to encrypt data
+		return "password";
+	}
+}
