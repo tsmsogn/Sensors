5a18261434bd0e7d0f3f243dc61bad718098f674
tsmsogn <tsmsogn@gmail.com>
2017-05-25 18:43:59 +0900

    Add default values to remote server settings

--- sensorManagerExample/src/main/java/com/ubhave/sensormanager/tester/RemoteServerActivity.java
+++ sensorManagerExample/src/main/java/com/ubhave/sensormanager/tester/RemoteServerActivity.java
@@ -26,23 +26,27 @@ public class RemoteServerActivity extends PreferenceActivity
 
 		CheckBoxPreference postFile = new CheckBoxPreference(this);
 		postFile.setKey(RemoteServerDetails.POST_FILE);
+		postFile.setDefaultValue(RemoteServerDetails.DEFAULT_VALUE_POST_FILE);
 		postFile.setTitle("Post data to remote server");
 		screen.addPreference(postFile);
 
 		EditTextPreference filePostURL = new EditTextPreference(this);
 		filePostURL.setKey(RemoteServerDetails.FILE_POST_URL);
+		filePostURL.setDefaultValue(RemoteServerDetails.DEFAULT_VALUE_FILE_POST_URL);
 		filePostURL.setTitle("URL");
 		filePostURL.setSummary("File post URL");
 		screen.addPreference(filePostURL);
 
 		EditTextPreference fileKey = new EditTextPreference(this);
 		fileKey.setKey(RemoteServerDetails.FILE_KEY);
+		fileKey.setDefaultValue(RemoteServerDetails.DEFAULT_VALUE_FILE_KEY);
 		fileKey.setTitle("Key");
 		fileKey.setSummary("Key of parameter which includes file data.");
 		screen.addPreference(fileKey);
 
 		EditTextPreference responseOnSuccess = new EditTextPreference(this);
 		responseOnSuccess.setKey(RemoteServerDetails.RESPONSE_ON_SUCCESS);
+		responseOnSuccess.setDefaultValue(RemoteServerDetails.DEFAULT_VALUE_RESPONSE_ON_SUCCESS);
 		responseOnSuccess.setTitle("Response data on success");
 		responseOnSuccess.setSummary("Whether or not the post has been completed successfully.");
 		screen.addPreference(responseOnSuccess);
--- sensorManagerExample/src/main/java/com/ubhave/sensormanager/tester/loggers/RemoteServerDetails.java
+++ sensorManagerExample/src/main/java/com/ubhave/sensormanager/tester/loggers/RemoteServerDetails.java
@@ -12,27 +12,32 @@ public class RemoteServerDetails
 	public static final String FILE_KEY = "FILE_KEY";
 	public static final String RESPONSE_ON_SUCCESS = "response_on_success";
 
+	public static final boolean DEFAULT_VALUE_POST_FILE = false;
+	public static final String DEFAULT_VALUE_FILE_POST_URL = "";
+	public static final String DEFAULT_VALUE_FILE_KEY = "file";
+	public static final String DEFAULT_VALUE_RESPONSE_ON_SUCCESS = "";
+
 	public static boolean postFile()
 	{
 		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(ApplicationContext.getContext());
-		return preferences.getBoolean(POST_FILE, false);
+		return preferences.getBoolean(POST_FILE, DEFAULT_VALUE_POST_FILE);
 	}
 
 	public static String getFilePostUrl()
 	{
 		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(ApplicationContext.getContext());
-		return preferences.getString(FILE_POST_URL, "");
+		return preferences.getString(FILE_POST_URL, DEFAULT_VALUE_FILE_POST_URL);
 	}
 
 	public static String getFileKey()
 	{
 		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(ApplicationContext.getContext());
-		return preferences.getString(FILE_KEY, "file");
+		return preferences.getString(FILE_KEY, DEFAULT_VALUE_FILE_KEY);
 	}
 
 	public static String getResponseOnSuccess()
 	{
 		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(ApplicationContext.getContext());
-		return preferences.getString(RESPONSE_ON_SUCCESS, "");
+		return preferences.getString(RESPONSE_ON_SUCCESS, DEFAULT_VALUE_RESPONSE_ON_SUCCESS);
 	}
 }
